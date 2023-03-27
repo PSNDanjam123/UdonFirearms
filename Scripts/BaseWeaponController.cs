0e799a39b3b6ef6d1f777ccbf51465c4299ff5b7
Jamie Bailey <psndanjam123@gmail.com>
2023-03-27 01:23:35 +0100

    working through animator

--- Scripts/BaseWeaponController.cs
+++ Scripts/BaseWeaponController.cs
@@ -3,6 +3,7 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRC.Udon.Common;
 
 [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class BaseWeaponController : UdonSharpBehaviour
@@ -10,6 +11,8 @@ public class BaseWeaponController : UdonSharpBehaviour
     [Header("Components")]
     [SerializeField]
     VRC_Pickup m_vRCPickup;
+    [SerializeField]
+    Animator m_animator;
     [SerializeField]
     Transform m_muzzle;
     [SerializeField]
@@ -21,6 +24,8 @@ public class BaseWeaponController : UdonSharpBehaviour
 
     [Header("State")]
     [SerializeField]
+    bool m_safety = false;
+    [SerializeField]
     bool m_triggerPulled;
     [SerializeField, Range(0, 255)]
     byte m_ammoCount = 0;
@@ -28,6 +33,11 @@ public class BaseWeaponController : UdonSharpBehaviour
     bool m_magazineLoaded = false;
     [SerializeField, Range(0, 2)]
     byte m_currentFireMode = 0;
+
+    [Header("Animator")]
+    [SerializeField]
+    bool m_animTogglingSafety = false;
+    bool m_animFiring = false;
 
 
     [Header("Ammunition")]
@@ -60,15 +70,90 @@ public class BaseWeaponController : UdonSharpBehaviour
 
     void Start()
     {
-        if (m_vRCPickup == null)
-        {
-            m_vRCPickup = GetComponent<VRC_Pickup>();
-        }
         m_vRCPickup.orientation = VRC_Pickup.PickupOrientation.Gun;
         m_vRCPickup.AutoHold = VRC_Pickup.AutoHoldMode.Yes;
         m_vRCPickup.ExactGun = m_grip.transform;
         m_grip.transform.rotation *= Quaternion.Euler(-90, 0, -90);
     }
 
+    void Update()
+    {
+        HandleInput();
+    }
+
+    public override void InputUse(bool value, UdonInputEventArgs args)
+    {
+        if (value)
+        {
+            Debug.Log("Bang!");
+            Fire();
+        }
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            ToggleSafety();
+        }
+    }
+
+    public void Fire()
+    {
+        if (AnimatorHas("Fire"))
+        {
+            if (m_animFiring)
+            {
+                return;
+            }
+            m_animator.SetBool("Fire", true);
+        }
+    }
+
+    public void ToggleSafety()
+    {
+        if (AnimatorHas("Safety"))
+        {
+            if (m_animTogglingSafety)
+            {
+                return;
+            }
+            m_animator.SetBool("Safety", !m_animator.GetBool("Safety"));
+        }
+        m_safety = !m_safety;
+    }
+
+    public void AnimFireStart()
+    {
+        m_animFiring = true;
+    }
+
+    public void AnimFireFinish()
+    {
+        m_animFiring = false;
+        m_animator.SetBool("Fire", false);
+    }
+
+    public void AnimTogglingSafetyStart()
+    {
+        m_animTogglingSafety = true;
+    }
+
+    public void AnimTogglingSafetyFinish()
+    {
+        m_animTogglingSafety = false;
+    }
+
+    bool AnimatorHas(string name)
+    {
+        foreach (var param in m_animator.parameters)
+        {
+            if (param.name == name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
